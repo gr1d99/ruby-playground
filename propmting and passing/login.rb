9bbe4d0d36bd05026a4b33e547f8b9f99c0df6af
Gideon Kimutai <gideonkimutai9@gmail.com>
2018-06-29 09:38:39 +0300

    propting and passing

---
+++ propmting and passing/login.rb
@@ -0,0 +1,8 @@
+username, password = ARGV
+
+USERNAME = 'gideon'.freeze
+PASSWORD = 'password'.freeze
+
+if username.eql?(USERNAME) && password.eql?(PASSWORD)
+  puts 'Whoa!!! you are now a hacker'
+end
